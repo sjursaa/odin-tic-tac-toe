f469d863f0dd3d71e49307258572c334cae796cf
Sjur <sjursaa@gmail.com>
2024-11-04 11:45:55 +0100

    implement create, update and display functions for gameboard, test in game function

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 console.log("hello world");
 
 const gameboard = (function () {
-  let gameboardArray = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-  ];
-  console.log(gameboardArray);
+  // let gameboardArray = [
+  //   [0, 0, 0],
+  //   [0, 0, 0],
+  //   [0, 0, 0],
+  // ];
+  // console.log(gameboardArray);
+  // create = [[0, 0, 0]]
+  const create = () =>
+    (gameboardArray = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]);
+  const update = (character, positionX, positionY) =>
+    (gameboardArray[positionX][positionY] = character);
+  const display = () => console.log(gameboardArray);
+  return { create, update, display };
 })();
 
 function playerX() {
   let character = "X";
+  let squaresOccupied = 0;
 }
 
 function playerO() {
   let character = "O";
 }
 
-gameboard();
+function game() {
+  // let currentTurn = playerX.character;
+  // console.log(playerX.character);
+  gameboard.create();
+  gameboard.update("X", 0, 0);
+  gameboard.display();
+}
+
+// gameboard();
+game();
