9b5f941de2507ef72c360bb0d611a144c53e55bd
Sjur <sjursaa@gmail.com>
2024-11-04 11:21:41 +0100

    add basic functions to script

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+function gameboard() {
+  let gameboardArray = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+  console.log(gameboardArray);
+}
+
+function playerX() {}
+
+function playerO() {}
+
+gameboard();
